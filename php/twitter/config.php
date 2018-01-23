acdd5431fc3d407db90cc1c20c1a3665830e6b20
Alec Dilanchian <alec.dilanchian@gmail.com>
2018-01-22 18:35:33 -0600

    Adding base website

---
+++ php/twitter/config.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Your Twitter App Info
+ * How to create Twitter API key you can read here (or use google):  - http://www.gabfirethemes.com/create-twitter-api-key/
+ */
+
+ // User Name
+ define('USER_NAME', 'TwitchTacos');
+
+// Consumer Key
+define('CONSUMER_KEY', 'oj4HulLALBBxwj9Wdg0q6u07T');
+define('CONSUMER_SECRET', '9UJGsjNMLtkZ8MlHq43i3nCf2KqnKWcqvzy9fC0Edjm0kOxnbl');
+
+// User Access Token
+define('ACCESS_TOKEN', '939218927179071488-twWJKp8U8kKyyQVZzEHOzRT2MjwaRAb');
+define('ACCESS_SECRET', 'sgQ5rOxBGr3AzxJNI2zmuJCEa8Tv38kDnwKC6YvEZ4dN6');
+
+// Cache Settings
+define('CACHE_ENABLED', true);
+define('CACHE_LIFETIME', 3600); // in seconds
+define('HASH_SALT', md5(dirname(__FILE__)));
